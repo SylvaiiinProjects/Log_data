716cab565c7325747cd9063a7f2dbaac2499a19a
Sylvaiiin <44996175+SylvaiiinProjects@users.noreply.github.com>
2018-12-06 09:38:20 +0100

    V2

--- Log_soil_data.py
+++ Log_soil_data.py
@@ -10,6 +10,7 @@ import numpy as np
 
 
 FARMWARE_NAME = 'log_sensor_data'
+HEADERS = {    'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),    'content-type': 'application/json'}
 
 
 def get_env(key, type_=int):
@@ -17,13 +18,10 @@ def get_env(key, type_=int):
     return type_(os.getenv('{}_{}'.format(FARMWARE_NAME, key), 64))
 
 def post(wrapped_data):
-    """Send the Celery Script command."""
-    headers = {
-        'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),
-        'content-type': 'application/json'}
+    """Send the Celery Script command."""   
     payload = json.dumps(wrapped_data)
     requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script',
-                  data=payload, headers=headers)
+                  data=payload, headers= HEADERS)
 
 def no_data_error():
     """Send an error to the log if there's no data."""
@@ -35,29 +33,36 @@ def no_data_error():
             'message': message}}
     post(wrapped_message)
 
-def get_pin_data(pin):
-    """Get existing historical pin data."""
-    data = json.loads(os.getenv('pin_data_' + str(pin), '[]'))
-    if len(data) < 1:
-        no_data_error()
-        sys.exit(0)
-    else:
-        return data
 
-def log_data():
-    """Send data."""
-    message = '[Log sensor data]  data available for pin {} is {}.'.format(PIN,d)
+
+def get_pin_value(pin): 
+   """Get the value read by a Sequence `Read Pin` step or the Sensor widget."""
+  
+  response = requests.get(  os.environ['FARMWARE_URL'] + 'api/v1/bot/state',   headers=HEADERS)
+    try:       
+	 		value = response.json()['pins'][str(pin)]['value']
+			
+    except KeyError:   
+   		  value = None
+		  
+     if value is None:   
+  	   no_data_error() 
+  	   sys.exit(0)
+	 
+	 else: 
+	 	message = '[Log sensor value] Value for pin {} is {}.'.format(PIN,value)
     wrapped_message = {
         'kind': 'send_message',
         'args': {
-            'message_type': 'log',
+            'message_type': 'error',
             'message': message}}
     post(wrapped_message)
-
+   
+   return 0
 
 
 
 if __name__ == '__main__':
     PIN = get_env('pin')
-    d= get_pin_data(PIN)
-    log_data()
+    get_pin_data(PIN)
+    
