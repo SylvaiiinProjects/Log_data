ee155049f064d3af3093e75c1bb9821f868c3775
Sylvaiiin <44996175+SylvaiiinProjects@users.noreply.github.com>
2018-12-10 11:37:09 +0100

    Add files via upload

--- Log_soil_data.py
+++ Log_soil_data.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python
 
-
-
-
 """Save sensor value."""
 
 import os
 import sys
 import json
 from time import time
-from functools import wraps
 import requests
 
 FARMWARE_NAME = 'Log Value'
@@ -48,38 +44,11 @@ def data(value):
             'message': message}}
     post(wrapped_message)
 
-def create_node(kind=None, args=None):
-    """Create a kind, args Celery Script node."""
-    node = {}
-    node['kind'] = kind
-    node['args'] = args
-    return node
-
-
-def read_pin(number=0, mode=0, label='---'):
-    """Celery Script to read the value of a pin.
-    Kind:
-        read_pin
-    Arguments:
-        pin_number: 0
-        pin_mode: 0 [0, 1]
-        label: '---'
-    """
-    args = {}
-    args['pin_number'] = number
-    args['pin_mode'] = mode
-    args['label'] = label
-    _read_pin = create_node(kind='read_pin', args=args)
-    return _read_pin
-
-
-
 def get_pin_value(pin):
     """ Sequence `Read Pin` """
     response = requests.get(
         os.environ['FARMWARE_URL'] + 'api/v1/bot/state',
         headers=HEADERS)
-    # bot/state endroit où sont stockées les données
     try:
         value = response.json()['pins'][str(pin)]['value']
     except KeyError:
@@ -96,28 +65,7 @@ def timestamp(value):
     """Add a timestamp to the pin value."""
     return {'time': time(), 'value': value}
 
-def _print_json(function):
-    @wraps(function)
-    def wrapper(*args, **kwargs):
-        """Send Celery Script or return the JSON string.
-        Celery Script is sent by sending an HTTP POST request to /celery_script
-        using the url in the `FARMWARE_URL` environment variable.
-        """
-        try:
-            os.environ['FARMWARE_URL']
-        except KeyError:
-            # Not running as a Farmware: return JSON
-            return function(*args, **kwargs)
-        else:
-            # Running as a Farmware: send Celery Script command
-            farmware_token = os.environ['FARMWARE_TOKEN']
-            headers = {'Authorization': 'bearer {}'.format(farmware_token),
-                       'content-type': "application/json"}
-            payload = json.dumps(function(*args, **kwargs))
-            ret = requests.post(farmware_api_url() + 'celery_script',
-                          data=payload, headers=headers)
-            return ret
-    return wrapper
+
 
 def post(wrapped_data):
     """Send the Celery Script command."""
@@ -128,11 +76,5 @@ def post(wrapped_data):
 if __name__ == '__main__':
     PIN = get_env('pin')    
     #PIN = get_input_env()
-    #data(post(read_pin(number=64,mode=0, label='Value is ')))
-    #pour afficher directement la valeur obtenue
-    data(_print_json(read_pin(number=64,mode=0, label='Value is ')))
     get_pin_value(PIN)
     
-
-
-
